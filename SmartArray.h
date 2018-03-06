e6ef677f5e040932c4ea816b8d1a0c7ef33e8072
Ray <yrq901207@gmail.com>
2018-03-06 10:06:27 -0500

    EOC W7

--- SmartArray.h
+++ SmartArray.h
@@ -13,25 +13,13 @@ namespace collections {
 
     class SmartArray
     {
-        long* m_pData;
+        float* m_pData;
         int m_size;
     public:
         SmartArray();
-        SmartArray(long item);
+        SmartArray(float);
         ~SmartArray();
-        void add(long item);
-        void display() const;
-        void display(std::ostream& out) const;
-        // overload operators
-        void operator+=(long item);
-
-        operator bool() const;
     };
-
-    // free helpers
-    // no const at the end of non member functions
-    std::ostream& operator<<(std::ostream& out, const SmartArray& arr);
-    // friend functions / friend classes
 
 }
 
--- main.cpp
+++ main.cpp
@@ -1,35 +1,11 @@
 #include <iostream>
-#include "car.h"
-#include "SmartArray.h"
 
 using namespace std;
-using namespace automotive;
-using namespace collections;
+
 
 int main()
 {
 
-    int week = 5;
-
-    if (week == 4) {
-        Car aCar(5, 35856.43, "Seneca"), anotherCar;
-        aCar.set(3, 3.4, "123");
-        aCar.display();
-    }
-
-    if (week == 5) {
-        SmartArray anArray;
-        // constructor overload
-        SmartArray anotherArray = 100;
-        if (anArray)
-            anArray.display(cout);
-        anArray.add(10);
-        anArray.add(20);
-        anArray.add(12);
-        anArray += 30;
-        cout << anArray << endl;
-    }
-
     return 0;
-    // destructor called here, LIFO
+
 }
